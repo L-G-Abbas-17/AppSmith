3b736c16e52e6dc834f2973dbdc0afac27be8d58
Abbas 17 <naasirn31@gmail.com>
2025-10-26 10:00:56 +0000

    secure 4

--- pages/Login/jsobjects/Loginbtnscript/Loginbtnscript.js
+++ pages/Login/jsobjects/Loginbtnscript/Loginbtnscript.js
@@ -2,7 +2,7 @@ export default {
 	LoginonClick() {
 		if(Email.text === "nova" && Password.text === "4321") {
 			// set login true + expiry timestamp
-			let expiryTime = new Date().getTime() + 2 * 60 * 1000; // 2 mins
+			let expiryTime = new Date().getTime() + 5 * 60 * 60 * 1000; // 5 hours in ms
 			storeValue("isLoggedIn", true);
 			storeValue("loginExpiry", expiryTime);
 			navigateTo('Dashboard');
