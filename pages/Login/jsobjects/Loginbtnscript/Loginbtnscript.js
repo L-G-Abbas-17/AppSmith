431f77e7297672c8ce332beb978da12e6d61a595
Abbas 17 <naasirn31@gmail.com>
2025-10-26 11:01:09 +0000

    remove experiy time

--- pages/Login/jsobjects/Loginbtnscript/Loginbtnscript.js
+++ pages/Login/jsobjects/Loginbtnscript/Loginbtnscript.js
@@ -2,9 +2,7 @@ export default {
 	LoginonClick() {
 		if(Email.text === "Nova" && Password.text === "Hash8labs$$25") {
 			// set login true + expiry timestamp
-			let expiryTime = new Date().getTime() + 5 * 60 * 60 * 1000; // 5 hours in ms
 			storeValue("isLoggedIn", true);
-			storeValue("loginExpiry", expiryTime);
 			navigateTo('Dashboard');
 		} else {
 			showAlert("Invalid User Name or Password", "error");
