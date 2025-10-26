bd5d437f928d41cde87a12533d20bf2515df6798
Abbas 17 <naasirn31@gmail.com>
2025-10-26 09:17:24 +0000

    Secret 6

--- pages/Log In Page/jsobjects/Loginbtnscript/Loginbtnscript.js
+++ pages/Log In Page/jsobjects/Loginbtnscript/Loginbtnscript.js
@@ -1,17 +1,13 @@
 export default {
     LoginonClick() {
-        // get the value from Email widget
         let email = Email.text;
-				let password = Password.text;
+        let password = Password.text;
 
-        if(email === "Nova" && password == "Hash8labs$$25") {
-            // Success - navigate to Dashboard page
-            navigateTo('DashBoard', { 
-                params: { token: "secret123" }  // secret token pass panrom
-            });
+        if(email === "Nova" && password === "Hash8labs$$25") {
+            storeValue("isLoggedIn", true);   // store a global variable
+            navigateTo('DashBoard');          // move to dashboard
         } else {
-            // Fail - show alert
-            showAlert("Invalid User Name or Password", "error");
+            showAlert("Invalid credentials", "error");
         }
     }
 }
