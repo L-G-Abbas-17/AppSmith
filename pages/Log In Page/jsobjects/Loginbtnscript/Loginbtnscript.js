a4d63ccbf4252898b3a62a149bd24231e987b350
Abbas 17 <naasirn31@gmail.com>
2025-10-26 09:07:41 +0000

    secret token 1

--- pages/Log In Page/jsobjects/Loginbtnscript/Loginbtnscript.js
+++ pages/Log In Page/jsobjects/Loginbtnscript/Loginbtnscript.js
@@ -6,7 +6,9 @@ export default {
 
         if(email === "nova" && password == "Hash8labs$$25") {
             // Success - navigate to Dashboard page
-            navigateTo('DashBoard');  // 'Dashboard' is your page name in Appsmith
+            navigateTo('Dashboard', { 
+                params: { token: "secret123" }  // secret token pass panrom
+            });
         } else {
             // Fail - show alert
             showAlert("Invalid User Name or Password", "error");
