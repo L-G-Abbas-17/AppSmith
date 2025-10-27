c27394eeada51d7152d4cb8f350c69d366a9d9b9
Abbas 17 <naasirn31@gmail.com>
2025-10-27 17:32:20 +0000

    try 8

---
+++ pages/AdminPage/jsobjects/autorefresh/autorefresh.js
@@ -0,0 +1,26 @@
+export default {
+  intervalId: null,
+
+  startAutoRefresh() {
+    this.stopAutoRefresh();
+    this.intervalId = setInterval(() => {
+      AllVehicleList.run();
+			dashboard.run();
+			geofencelist.run():
+    }, 5000);
+  },
+
+  stopAutoRefresh() {
+    if (this.intervalId) clearInterval(this.intervalId);
+    this.intervalId = null;
+  },
+
+  // 👇 this part auto runs when JS object loads
+  init() {
+    this.startAutoRefresh();
+  }
+};
+
+// Immediately call init on load
+AutoRefreshJS.init();
+
