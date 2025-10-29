01a2f880acbdfb1e48a081bb178186990ea8d941
Abbas 17 <naasirn31@gmail.com>
2025-10-29 15:52:42 +0000

    try 0

--- pages/UserPage/jsobjects/SaveSomeinfo/SaveSomeinfo.js
+++ pages/UserPage/jsobjects/SaveSomeinfo/SaveSomeinfo.js
@@ -1,15 +1,23 @@
 export default {
-	savesomeinfo(){
+	savesomeinfo(what_return){
 		vehicleinfo.run()
 		const vehicleinformation = vehicleinfo.data[0];
 		console.log(vehicleinformation,"jhjh")
-		
+
 		let GefenceName = vehicleinformation.GeoFenceName;
 		let Address =vehicleinformation.Address;
-		let speed = vehicleinformation.speed;
-		
-		return GefenceName;
-		
-		
+		let Speed = vehicleinformation.speed;
+
+		if (what_return == "GeofenceName"){
+			return GefenceName;
+		}
+		else if (what_return == "Address"){
+			return Address;
+		}
+		else if (what_return == "Speed"){
+			return Speed;
+		}
+
 	}
 }
+//{{vehicleinfo.data[0]?.GeoFenceName? vehicleinfo.data[0]?.GeoFenceName:"Location Not Found"}}
