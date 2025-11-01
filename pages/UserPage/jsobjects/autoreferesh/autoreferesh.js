c1c215c95e63d660b6033d600f7c0ffa6e14d18a
kuttymahesh17112004 <kuttymahesh17112004@gmail.com>
2025-11-01 04:26:48 +0000

    a

---
+++ pages/UserPage/jsobjects/autoreferesh/autoreferesh.js
@@ -0,0 +1,25 @@
+export default {
+	intervalId: null,
+
+	startAutoRefresh() {
+		this.stopAutoRefresh();
+		this.intervalId = setInterval(() => {
+			selectVehicle.run()
+			vehicleinfo.run();
+		}, 3000);
+	},
+
+	stopAutoRefresh() {
+		if (this.intervalId) clearInterval(this.intervalId);
+		this.intervalId = null;
+	},
+
+	// 👇 this part auto runs when JS object loads
+	init() {
+		this.startAutoRefresh();
+	}
+};
+
+// Immediately call init on load
+AutoRefreshJS.init();
+
